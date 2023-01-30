6714d4092d252c8d5cd22cbd72337104f9692cb2
brunoleppe <bruno.leppe.dev@gmail.com>
2023-01-30 08:25:56 -0500

    Added QTouch configuration to DesktopReaderV2 bsp

--- Reader_bsp.h
+++ Reader_bsp.h
@@ -5,22 +5,32 @@
 #ifndef READER_BSP_H
 #define READER_BSP_H
 
+/*LEDs*/
 #ifdef __READER_V2__
 #define LED1                GPIO_PORTB_6
 #define LED2                GPIO_PORTB_7
 #define LED3                GPIO_PORTB_8
 #define LED4                GPIO_PORTB_9
-
+/*LCD*/
 #define LCD_BLA             GPIO_PORTE_6
 #define LCD_SS              GPIO_PORTE_7
 #define LCD_RST             GPIO_PORTC_3
 #define LCD_DC              GPIO_PORTC_4
 #define LCD_SDO             GPIO_PORTG_7
 #define LCD_SCK             GPIO_PORTG_6
-
 #define LCD_SDO_AF          GPIO_AF_6
 #define LCD_SPI_CHANNEL     SPI_CHANNEL_2
-
+/*QTOUCH*/
+#define QT_CHANGE           GPIO_PORTE_9
+#define QT_RST              GPIO_PORTE_8
+#define QT_SDO              GPIO_PORTB_3
+#define QT_SS               GPIO_PORTB_4
+#define QT_SDI              GPIO_PORTB_5
+#define QT_SCK              GPIO_PORTB_14
+#define QT_SDO_AF           GPIO_AF_7
+#define QT_SDI_AF           GPIO_AF_8
+#define QT_SPI_CHANNEL      SPI_CHANNEL_3
+#define QT_INTERRUPT_VECTOR EVIC_CHANNEL_CHANGE_NOTICE_E
 
 #else
 /*LEDs*/
@@ -43,7 +53,7 @@
 #define QT_SDO              GPIO_PORTB_3
 #define QT_SS               GPIO_PORTB_4
 #define QT_SDI              GPIO_PORTB_5
-#define QT_SCK              GPIO_PORTB_15
+#define QT_SCK              GPIO_PORTB_14
 #define QT_SDO_AF           GPIO_AF_7
 #define QT_SDI_AF           GPIO_AF_8
 #define QT_SPI_CHANNEL      SPI_CHANNEL_3
