6709d640217c4002eb05afc1e75e2d1768e92924
brunoleppe <bruno.leppe.dev@gmail.com>
2023-02-02 12:16:46 -0500

    Updates

--- HAL/gpio.h
+++ HAL/gpio.h
@@ -20,7 +20,51 @@
 /**********************************************************************
 * Preprocessor Constants
 **********************************************************************/
+#define _GPIO_PA         (GPIO_Descriptor)(&ANSELA)
+#define _GPIO_PB         (GPIO_Descriptor)(&ANSELB)
+#define _GPIO_PC         (GPIO_Descriptor)(&ANSELC)
+#define _GPIO_PD         (GPIO_Descriptor)(&ANSELD)
+#define _GPIO_PE         (GPIO_Descriptor)(&ANSELE)
+#define _GPIO_PF         (GPIO_Descriptor)(&ANSELF)
+#define _GPIO_PG         (GPIO_Descriptor)(&ANSELG)
+#define _GPIO_PH         (GPIO_Descriptor)(&ANSELH)
+#define _GPIO_PJ         (GPIO_Descriptor)(&ANSELJ)
+#define _GPIO_PK         (GPIO_Descriptor)(&ANSELK)
 
+#define _GPIO_INPUT      0x0000
+#define _GPIO_OUTPUT     0x0001
+#define _GPIO_ANALOG     0x0002
+#define _GPIO_PULLUP     0x0004
+#define _GPIO_PULLDOWN   0x0008
+#define _GPIO_OPENDRAIN  0x0010
+#define _GPIO_SLOWEST    0x0020
+#define _GPIO_SLOW       0x0040
+#define _GPIO_FAST       0x0080
+#define _GPIO_FASTEST    0x0100
+#define _GPIO_IRQ        0x0200
+
+#define _GPIO_INPUT_PULLUP       (_GPIO_PULLUP)
+#define _GPIO_INPUT_PULLDOWN     (_GPIO_PULLDOWN)
+#define _GPIO_OUTPUT_OD          (_GPIO_OUTPUT | _GPIO_OPENDRAIN)
+
+
+#define _GPIO_PIN_0                 ((uint16_t)0x0001)  /* Pin 0 selected    */
+#define _GPIO_PIN_1                 ((uint16_t)0x0002)  /* Pin 1 selected    */
+#define _GPIO_PIN_2                 ((uint16_t)0x0004)  /* Pin 2 selected    */
+#define _GPIO_PIN_3                 ((uint16_t)0x0008)  /* Pin 3 selected    */
+#define _GPIO_PIN_4                 ((uint16_t)0x0010)  /* Pin 4 selected    */
+#define _GPIO_PIN_5                 ((uint16_t)0x0020)  /* Pin 5 selected    */
+#define _GPIO_PIN_6                 ((uint16_t)0x0040)  /* Pin 6 selected    */
+#define _GPIO_PIN_7                 ((uint16_t)0x0080)  /* Pin 7 selected    */
+#define _GPIO_PIN_8                 ((uint16_t)0x0100)  /* Pin 8 selected    */
+#define _GPIO_PIN_9                 ((uint16_t)0x0200)  /* Pin 9 selected    */
+#define _GPIO_PIN_10                ((uint16_t)0x0400)  /* Pin 10 selected   */
+#define _GPIO_PIN_11                ((uint16_t)0x0800)  /* Pin 11 selected   */
+#define _GPIO_PIN_12                ((uint16_t)0x1000)  /* Pin 12 selected   */
+#define _GPIO_PIN_13                ((uint16_t)0x2000)  /* Pin 13 selected   */
+#define _GPIO_PIN_14                ((uint16_t)0x4000)  /* Pin 14 selected   */
+#define _GPIO_PIN_15                ((uint16_t)0x8000)  /* Pin 15 selected   */
+#define _GPIO_PIN_All               ((uint16_t)0xFFFF)  /* All pins selected */
 /**********************************************************************
 * Typedefs
 **********************************************************************/
