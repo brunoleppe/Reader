9cb481d90349c63ae45614538d4cb509f369a9fd
brunoleppe <bruno.leppe.dev@gmail.com>
2023-03-16 16:29:13 -0500

    updated main.c

--- main.c
+++ main.c
@@ -1,15 +1,13 @@
 #include "initialization.h"
 #include "FreeRTOS.h"
 #include "task.h"
-#include "gpio.h"
-#include "spi.h"
 #include "Reader_bsp.h"
 #include "lcd.h"
 #include "bitmap.h"
 #include "hal_delay.h"
 
 typedef struct{
-    GPIO_PIN pin_number;
+    GPIO_PinMap pinMap;
     int delay_ms;
 }PinParams;
 
@@ -17,19 +15,27 @@ static void blink(void *params);
 static void lcd_task(void *params);
 static void qt_task(void *params);
 
-int main(void){
+static const PinParams pinParamsLed1 = {
+        .pinMap = LED1,
+        .delay_ms = 500
+};
+static const PinParams pinParamsLed2 = {
+        .pinMap = LED2,
+        .delay_ms = 500
+};
 
+
+int main(){
     /*Initialize processor*/
     Initialize();
     /*Create a FreeRTOS Task*/
-    xTaskCreate(blink,"blink_task",256,&(PinParams){.pin_number = LED1, .delay_ms = 500},1,NULL);
-    xTaskCreate(blink,"blink_task",256,&(PinParams){.pin_number = LED2, .delay_ms = 500},1,NULL);
+    xTaskCreate(blink,"blink_task",256,(void*)&pinParamsLed1,1,NULL);
+    xTaskCreate(blink,"blink_task",256,(void*)&pinParamsLed2,1,NULL);
 
-
-    xTaskCreate(lcd_task, "lcd_task", 1024, NULL, 3, NULL);
+    xTaskCreate(lcd_task, "lcd_task", 2048, NULL, 3, NULL);
     xTaskCreate(qt_task, "qt_task", 2048, NULL, 1, NULL);
 
-    while(1){
+    while(true){
         /*Start FreeRTOS scheduler*/
         vTaskStartScheduler();
     }
@@ -39,28 +45,26 @@ int main(void){
 
 static void qt_task(void *params)
 {
-    SPI_Handler __spi = &(struct  _SPI_Handler){
-        .usDelay = 160
-    };
-    SPI_Setup setup = {
-            .baudrate = 1000000,
-            .mastermode = SPI_MASTER,
-            .mode = SPI_MODE_3,
-            .sample = SPI_SAMPLE_END,
-    };
-    SPI_Init(__spi, QT_SPI_CHANNEL, &setup);
+    (void)params;
+//    static SPI_TransferSetup setup;
+//    setup.usDelay = 160;
     GPIO_pin_write(QT_RST, GPIO_LOW);
     vTaskDelay(10);
     GPIO_pin_write(QT_RST, GPIO_HIGH);
     vTaskDelay(100);
 
-    while(1){
-        vTaskDelay(20);
+    while(true){
+        vTaskDelay(500);
         if(GPIO_pin_read(QT_CHANGE) == GPIO_LOW)
         {
             uint8_t read_key[] = {0xC1, 0x00, 0x00};
             GPIO_pin_write(QT_SS, GPIO_LOW);
-            SPI_Transfer(__spi, read_key, NULL, sizeof(read_key));
+            SPI_byte_transfer(QT_SPI_CHANNEL, read_key[0]);
+            HAL_delay_us(160);
+            SPI_byte_transfer(QT_SPI_CHANNEL, read_key[1]);
+            HAL_delay_us(160);
+            SPI_byte_transfer(QT_SPI_CHANNEL, read_key[2]);
+            HAL_delay_us(160);
             GPIO_pin_write(QT_SS, GPIO_HIGH);
             GPIO_pin_toggle(LED3);
         }
@@ -70,47 +74,33 @@ static void qt_task(void *params)
 
 static void lcd_task(void *params)
 {
-    SPI_Handler __spi = &(struct _SPI_Handler){};
-    SPI_Setup setup = {
-            .baudrate = 20000000,
-            .mastermode = SPI_MASTER,
-            .mode = SPI_MODE_3,
-            .sample = SPI_SAMPLE_MID,
-    };
-    SPI_Init(__spi, LCD_SPI_CHANNEL, &setup);
-    LCD_init(__spi, LCD_SS, LCD_BLA, LCD_DC, LCD_RST);
+    (void)params;
+    LCD_init(LCD_SPI_CHANNEL, LCD_SS, LCD_BLA, LCD_DC, LCD_RST);
 
     LCD_draw_bitmap(0,0,bitmap,sizeof(bitmap));
-    LCD_draw_string(10,10,"Hola Mundo",LCD_FONT_MEDIUM,0xf);
+    char *s = "Hola Mundo";
+    LCD_draw_string(0,1,(char*)s,LCD_FONT_MEDIUM,LCD_COLOR_BLACK);
 
     int x=0,y=0;
-    while(1){
+    while(true){
         LCD_print();
         vTaskDelay(17);
         GPIO_pin_toggle(LCD_SS);
-        LCD_draw_point(x++,y,0xf);
+        LCD_draw_point(x++,y,LCD_COLOR_BLACK);
         if(x>=240){
             x=0;
             if(y++>=128)
                 y=0;
         }
     }
-//        LCD_print();
-//        vTaskDelay(500);
-//        GPIO_pin_toggle(LCD_SS);
-//        LCD_print();
-//        vTaskDelay(500);
-//        GPIO_pin_toggle(LCD_SS);
-//        vTaskDelay(portMAX_DELAY);
-
 }
 
 static void blink(void *params)
 {
     PinParams *p = (PinParams*)params;
-    while(1){
+    while(true){
         /*Toggle pin B6 every ~500ms*/
-        GPIO_pin_toggle(p->pin_number);
+        GPIO_pin_toggle(p->pinMap);
         vTaskDelay(p->delay_ms);
     }
 }
