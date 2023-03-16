c6474656c7f91999889d53bd2147d1baf4764071
brunoleppe <bruno.leppe.dev@gmail.com>
2023-03-16 16:27:57 -0500

    Added DMA Channel 0 interrupt. Removed SPI2 RX and TX interrupts.

--- interrupts.c
+++ interrupts.c
@@ -6,3 +6,6 @@
 #include <sys/attribs.h>
 
 void __ISR(_CHANGE_NOTICE_E_VECTOR, ipl0AUTO) PORTE_Change_Notice_IRQ( void );
+void __ISR(_DMA0_VECTOR, ipl2AUTO) DMA0_IRQ( void );
+//void __ISR(_SPI2_RX_VECTOR, ipl2AUTO) SPI2_RX_IRQ( void );
+//void __ISR(_SPI2_TX_VECTOR, ipl2AUTO) SPI2_TX_IRQ( void );
