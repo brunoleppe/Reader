e24c656235a59cd5dfd2df350e3bcf236b98f528
brunoleppe <bruno.leppe.dev@gmail.com>
2023-03-20 15:55:40 -0500

    New files qtouch.c, qtouch.h

---
+++ Drivers/qtouch.c
@@ -0,0 +1,5 @@
+//
+// Created by bleppe on 20/03/23.
+//
+
+#include "qtouch.h"
---
+++ Drivers/qtouch.h
@@ -0,0 +1,8 @@
+//
+// Created by bleppe on 20/03/23.
+//
+
+#ifndef READER_QTOUCH_H
+#define READER_QTOUCH_H
+
+#endif //READER_QTOUCH_H
