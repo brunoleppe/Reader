71318188be4bfe30353e11b89ffbf92b600c6aab
brunoleppe <bruno.leppe.dev@gmail.com>
2023-04-27 16:30:14 -0500

    new TCP socket task for Reader

---
+++ BSP/Reader/Socket/reader_socket.h
@@ -0,0 +1,12 @@
+#include <features.h>
+//
+// Created by bleppe on 27/04/23.
+//
+
+#ifndef READER_READER_SOCKET_H
+#define READER_READER_SOCKET_H
+
+
+_Noreturn void reader_socket_task(void *params);
+
+#endif //READER_READER_SOCKET_H
